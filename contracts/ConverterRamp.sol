f994e449e70a989ea39d8ee28d8a9138c0f70563
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2019-07-23 12:44:47 -0300

    refactor ConverterRamp contract

--- contracts/ConverterRamp.sol
+++ contracts/ConverterRamp.sol
@@ -6,12 +6,14 @@ import './interfaces/diaspore/LoanManager.sol';
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import './interfaces/token/TokenConverter.sol';
 import './interfaces/RateOracle.sol';
+import './safe/SafeERC20.sol';
 import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 
 
 contract ConverterRamp is Ownable {
     using SafeMath for uint256;
+    using SafeERC20 for IERC20;
 
     address public constant ETH_ADDRESS = address(0x00eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee);
 
@@ -45,18 +47,18 @@ contract ConverterRamp is Ownable {
         );
         
         (uint256 tokenCost, uint256 etherCost) = getCost(amount, _converter, _fromToken, address(token));
-        pullAmount(_fromToken, tokenCost, etherCost);
+        require(IERC20(_fromToken).safeTransferFrom(msg.sender, address(this), tokenCost), 'Error pulling token amount');
 
         // Convert using token _converter
         convertSafe(_converter, _fromToken, address(token), amount, tokenCost, etherCost);
 
         // Pay loan
         DebtEngine debtEngine = DebtEngine(_debtEngineAddress);
-        require(token.approve(_debtEngineAddress, amount), "Error on payment approve");
+        require(token.safeApprove(_debtEngineAddress, amount), "Error on payment approve");
         debtEngine.pay(_requestId, amount, _payFrom, _oracleData);
+        
         require(token.approve(_debtEngineAddress, 0), "Error removing the payment approve");
-        
-        
+        require(token.balanceOf(address(this)) == 0, "The contract balance should be zero");  
     }
 
     /*
@@ -87,13 +89,13 @@ contract ConverterRamp is Ownable {
 
         // Pull required _fromToken amount to sell
         (uint256 tokenCost, uint256 etherCost) = getCost(amount, _converter, _fromToken, address(token));
-        pullAmount(_fromToken, tokenCost, etherCost);
+        require(IERC20(_fromToken).safeTransferFrom(msg.sender, address(this), tokenCost), 'Error pulling token amount');
 
         // Convert _fromToken into RCN
         convertSafe(_converter, _fromToken, address(token), amount, tokenCost, etherCost);
 
         // Lend loan
-        require(token.approve(_loanManagerAddress, tokenCost), 'Error approving lend token transfer');
+        require(token.safeApprove(_loanManagerAddress, tokenCost), 'Error approving lend token transfer');
         LoanManager(_loanManagerAddress).lend(
             _requestId, 
             _oracleData, 
@@ -102,22 +104,21 @@ contract ConverterRamp is Ownable {
             _cosignerData, 
             _callbackData
         );
-        require(token.approve(_loanManagerAddress, 0), 'Error removing approve');
         
         // Transfer loan to msg.sender
         // DebtEngine(_debtEngineAddress).transferFrom(address(this), msg.sender, uint256(_requestId));
 
-        // The contract balance should remain the same
-        require(token.balanceOf(address(this)) == 0, 'The contract balance should not change');
+        require(token.safeApprove(_loanManagerAddress, 0), 'Error removing approve');
+        require(token.balanceOf(address(this)) == 0, 'The contract balance should be zero');
 
     }
 
     function getCost(uint _amount, address _converter, address _fromToken, address _token) public view returns (uint256, uint256)  {
         TokenConverter tokenConverter = TokenConverter(_converter);
-        if (_fromToken != ETH_ADDRESS) {
-            return tokenConverter.getPrice(_fromToken, _token, _amount);
-        } else {
+        if (_fromToken == ETH_ADDRESS) {
             return tokenConverter.getPrice(_token, _amount);
+        } else {
+            return tokenConverter.getPrice(_fromToken, _token, _amount);
         }
     }
 
@@ -128,6 +129,50 @@ contract ConverterRamp is Ownable {
         Handles, internally, ETH convertions
     */
     function convertSafe(
+        address _converter,
+        address _fromToken,
+        address _token,
+        uint256 _amount,
+        uint256 _tokenCost,
+        uint256 _etherCost
+    ) internal returns (uint256 bought) {
+        if (_fromToken == ETH_ADDRESS) {
+            ethConvertSafe(_converter, _fromToken, address(_token), _amount, _tokenCost, _etherCost);
+        } else {
+            tokenConvertSafe(_converter, _fromToken, address(_token), _amount, _tokenCost, _etherCost);
+        }
+    }
+
+    function tokenConvertSafe(
+        address _converter,
+        address _fromTokenAddress,
+        address _toTokenAddress,
+        uint256 _amount,
+        uint256 _tokenCost,
+        uint256 _etherCost
+    ) internal returns (uint256 bought) {
+        
+        IERC20 fromToken = IERC20(_fromTokenAddress);
+        IERC20 toToken = IERC20(_toTokenAddress);
+        TokenConverter tokenConverter = TokenConverter(_converter);
+
+        // safe approve tokens to tokenConverter
+        require(fromToken.safeApprove(address(tokenConverter), _tokenCost), 'Error approving token transfer');
+
+        // store the previus balance to validate after convertion
+        uint256 prevBalance = toToken.balanceOf(address(this));
+
+        // call convert in token converter
+        tokenConverter.convert(fromToken, toToken, _amount, _tokenCost, _etherCost, msg.sender);
+
+        // token balance should have increased by amount
+        require(_amount == toToken.balanceOf(address(this)) - prevBalance, 'Bought amound does does not match');
+
+        // If we are converting from a token, remove the approve
+        require(fromToken.safeApprove(address(tokenConverter), 0), 'Error removing token approve');
+    }
+
+    function ethConvertSafe(
         address _converter,
         address _fromTokenAddress,
         address _toTokenAddress,
@@ -140,50 +185,16 @@ contract ConverterRamp is Ownable {
         IERC20 toToken = IERC20(_toTokenAddress);
         TokenConverter tokenConverter = TokenConverter(_converter);
 
-        // If we are converting from ETH, we don't need to approve the converter
-        if (_toTokenAddress != ETH_ADDRESS) {
-            require(fromToken.approve(address(tokenConverter), _tokenCost), 'Error approving token transfer');
-        }
+        // store the previus balance to validate after convertion
+        uint256 prevBalance = address(this).balance;
 
-        // Store the previus balance to validate after convertion
-        uint256 prevBalance = _toTokenAddress != ETH_ADDRESS ? toToken.balanceOf(address(this)) : address(this).balance;
+        // call convert in token converter
+        tokenConverter.convert.value(_amount)(fromToken, toToken, _amount, _tokenCost, _etherCost, msg.sender);
 
-        // Call convert in token converter
-        uint256 sendEth = _fromTokenAddress == ETH_ADDRESS ? _amount : 0;
-        tokenConverter.convert.value(sendEth)(fromToken, toToken, _amount, _tokenCost, _etherCost, msg.sender);
-
-        // toToken balance should have increased by _amount
-        require(
-            _amount == (_toTokenAddress != ETH_ADDRESS ? toToken.balanceOf(address(this)) : address(this).balance) - prevBalance,
-            'Bought amound does does not match'
-        );
-
-        // If we are converting from a token, remove the approve
-        if (_fromTokenAddress != ETH_ADDRESS) require(fromToken.approve(address(tokenConverter), 0), 'Error removing token approve');
+        // token balance should have increased by amount
+        require(_amount == address(this).balance - prevBalance, 'Bought amound does does not match');
 
     }
-
-    /*
-        Withdraw tokens stalled in the contract
-    */
-    function withdrawTokens(
-        IERC20 _token,
-        address _to,
-        uint256 _amount
-    ) external onlyOwner returns (bool) {
-        return _token.transfer(_to, _amount);
-    }
-
-    /*
-        Withdraw ether stalled in the contract
-    */
-    function withdrawEther(
-        address payable _to,
-        uint256 _amount
-    ) external onlyOwner {
-        _to.transfer(_amount);
-    }
-
 
     /*
         Returns how much RCN is required for a given lend
@@ -234,49 +245,13 @@ contract ConverterRamp is Ownable {
         uint256 _amount,
         bytes memory _oracleData
     ) internal returns (uint256) {
-        if (_oracle == address(0)) return _amount;
+        if (_oracle == address(0)) {
+            return _amount;
+        }
         (uint256 tokens, uint256 equivalent) = RateOracle(_oracle).readSample(_oracleData);
 
         emit ReadedOracle(_oracle, tokens, equivalent);
         return tokens.mul(_amount) / equivalent;
-    }
-
-    /*
-        Pulls an _amount in _token or eth from the msg.sender
-        @dev If ETH, returns the excedent
-    */
-    function pullAmount(
-        address _token,
-        uint256 _tokenCost,
-        uint256 _etherCost
-    ) private {
-        // Handle both ETH and tokens
-        if (_token == ETH_ADDRESS) {
-            // If ETH, require msg.value to be at least the required _etherCost
-            require(msg.value >= _etherCost, 'Error pulling ETH etherCost');
-            // Return any exceding ETH, if any
-            if (msg.value > _etherCost) {
-                msg.sender.transfer(msg.value - _etherCost);
-            }
-        } else {
-            // If tokens, only perform a transferFrom
-            require(IERC20(_token).transferFrom(msg.sender, address(this), _tokenCost), 'Error pulling token amount');
-        }
-    }
-
-    /*
-        Transfers token or ETH
-    */
-    function transfer(
-        IERC20 _token,
-        address payable _to,
-        uint256 _amount
-    ) private {
-        if (address(_token) == ETH_ADDRESS) {
-            _to.transfer(_amount);
-        } else {
-            require(_token.transfer(_to, _amount), 'Error sending tokens');
-        }
     }
 
 }
