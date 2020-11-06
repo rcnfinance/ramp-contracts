41c1539f600838ad2b33d7c52ce1499980cd81dc
rotciv <victorfage@gmail.com>
2020-11-06 19:36:03 -0300

    fix js lint

--- truffle.js
+++ truffle.js
@@ -1,16 +1,16 @@
 module.exports = {
-    compilers: {
-        solc: {
-            version: '0.6.6',
-            docker: false,
-            settings: {
-                optimizer: {
-                    enabled: true,
-                    runs: 200,
-                },
-                evmVersion: 'istanbul',
-            },
+  compilers: {
+    solc: {
+      version: '0.6.6',
+      docker: false,
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
         },
+        evmVersion: 'istanbul',
+      },
     },
-    plugins: ['solidity-coverage'],
+  },
+  plugins: ['solidity-coverage'],
 };
