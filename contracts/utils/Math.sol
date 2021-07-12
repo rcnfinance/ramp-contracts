f6a2ef9efd4be7e8e0ec0004c21ca153319dbe11
rotcivegaf <victorfage@gmail.com>
2021-07-12 15:46:21 +0200

    min in one line

--- contracts/utils/Math.sol
+++ contracts/utils/Math.sol
@@ -3,11 +3,7 @@ pragma solidity ^0.8.0;
 
 library Math {
     function min(uint256 _a, uint256 _b) internal pure returns (uint256) {
-        if (_a < _b) {
-            return _a;
-        } else {
-            return _b;
-        }
+        return _a < _b ? _a : _b;
     }
 
     function divCeil(uint256 _a, uint256 _b) internal pure returns (uint256 c) {
